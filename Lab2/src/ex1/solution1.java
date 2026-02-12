70cc2af2ef31fc82e4279022a3e54f1554f4262b
Manjula <mguneratne3@gmail.com>
2026-02-12 11:46:43 -0500

    Lab2 Ex1 code added

---
+++ Lab2/src/ex1/solution1.java
@@ -0,0 +1,60 @@
+
+public static void main(String[] args) {
+    //a)
+    int n = 1000;    //T(n) = 1
+    System.out.println("Hey - your input is: " + n);   //T(n) = 2
+    System.out.println("Hmm.. I'm doing more stuff with: " + n);    //T(n) = 2
+    System.out.println("And more: " + n);    //T(n) = 2
+
+    //T(n) = 7
+    //O(1)     Constant-time algorithm
+
+    //b)
+    for (int i = 1; i < n; i = i * 2){   //T(n) = n
+        System.out.println("Hey - I'm busy looking at: " + i);   //T(n) = 2
+    }
+
+    //T(n) = n + 2
+    //O(n)     Linear-time algorithm
+
+    //c)
+    for (int i = 1; i <= n; i++){            //T(n) = n
+        for(int j = 1; j < n; j = j * 2) {       //T(n) = n
+            System.out.println("Hey - I'm busy looking at: " + i + " and " + j); //T(n) = 3
+        }
+    }
+
+    //T(n)=n2 + 2
+    //O(n2)     Quadratic-time algorithm
+
+    //d)
+    public static double[] prefixAverage1(double[] x) {
+        int n = x.length;                //T(n) = 1
+        double[] a = new double[n];      //T(n) = 1
+        for (int j=0; j < n; j++) {      //T(n) = n
+            double total = 0;             //T(n) = 1
+            for (int i=0; i <= j; i++)   //T(n) = n
+            total += x[i];               //T(n) = 1
+            a[j] = total / (j+1);         //T(n) = 1
+        }
+        return a;                        //T(n) = 1
+    }
+
+    //T(n)=n2 + 6
+    //O(n2)     Quadratic-time algorithm
+
+    //e)
+    public static double[] prefixAverage2(double[] x) {
+        int n = x.length;              //T(n) = 1
+        double[] a = new double[n];    //T(n) = 1
+        double total = 0;              //T(n) = 1
+        for (int j=0; j < n; j++) {    //T(n) = n
+            total += x[j];              //T(n) = 1
+            a[j] = total / (j+1);       //T(n) = 1
+        }
+        return a;                       //T(n) = 1
+    }
+
+    //T(n)=n + 6
+    //O(n)     Linear-time algorithm
+}

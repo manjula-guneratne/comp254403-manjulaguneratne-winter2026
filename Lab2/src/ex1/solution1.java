3bc4b8f38e3dbb4e6ee1a42e18080bf4740cc5a6
Manjula Guneratne <mguneratne3@gmail.com>
2026-02-12 20:21:48 -0500

    v1 update approved

--- Lab2/src/ex1/solution1.java
+++ Lab2/src/ex1/solution1.java
@@ -1,41 +1,51 @@
+package ex1;
 
-public static void main(String[] args) {
-    //a)
-    int n = 1000;    //T(n) = 1
-    System.out.println("Hey - your input is: " + n);   //T(n) = 2
-    System.out.println("Hmm.. I'm doing more stuff with: " + n);    //T(n) = 2
-    System.out.println("And more: " + n);    //T(n) = 2
+public class Solution1 {
 
-    //T(n) = 7
-    //O(1)     Constant-time algorithm
+    public static void sol_A(){
 
-    //b)
-    for (int i = 1; i < n; i = i * 2){   //T(n) = n
-        System.out.println("Hey - I'm busy looking at: " + i);   //T(n) = 2
+        int n = 1000;    //T(n) = 1
+        System.out.println("Hey - your input is: " + n);   //T(n) = 2
+        System.out.println("Hmm.. I'm doing more stuff with: " + n);    //T(n) = 2
+        System.out.println("And more: " + n);    //T(n) = 2
+
+        //        T(n) = 7
+        //        O(1)     Constant-time algorithm
     }
 
-    //T(n) = n + 2
-    //O(n)     Linear-time algorithm
+    public static void sol_B(){
 
-    //c)
-    for (int i = 1; i <= n; i++){            //T(n) = n
-        for(int j = 1; j < n; j = j * 2) {       //T(n) = n
-            System.out.println("Hey - I'm busy looking at: " + i + " and " + j); //T(n) = 3
+        int n = 5;
+        for (int i = 1; i < n; i = i * 2){   //T(n) = n
+            System.out.println("Hey - I'm busy looking at: " + i);   //T(n) = 2
         }
+
+        //(n) = n + 2
+        //O(n)     Linear-time algorithm
+
     }
 
-    //T(n)=n2 + 2
-    //O(n2)     Quadratic-time algorithm
+    public static void sol_C(){
 
-    //d)
+        int n = 5;
+        for (int i = 1; i <= n; i++){            //T(n) = n
+            for(int j = 1; j < n; j = j * 2) {       //log(n) = n
+                System.out.println("Hey - I'm busy looking at: " + i + " and " + j); //T(n) = 3
+            }
+        }
+
+        //T(n)=nlog(n) + 3
+        //O(nlog(n))     logrithmic-time algorithm
+    }
+
     public static double[] prefixAverage1(double[] x) {
         int n = x.length;                //T(n) = 1
         double[] a = new double[n];      //T(n) = 1
         for (int j=0; j < n; j++) {      //T(n) = n
-            double total = 0;             //T(n) = 1
+            double total = 0;            //T(n) = 1
             for (int i=0; i <= j; i++)   //T(n) = n
             total += x[i];               //T(n) = 1
-            a[j] = total / (j+1);         //T(n) = 1
+            a[j] = total / (j+1);        //T(n) = 1
         }
         return a;                        //T(n) = 1
     }
@@ -43,18 +53,22 @@ public static void main(String[] args) {
     //T(n)=n2 + 6
     //O(n2)     Quadratic-time algorithm
 
-    //e)
     public static double[] prefixAverage2(double[] x) {
         int n = x.length;              //T(n) = 1
         double[] a = new double[n];    //T(n) = 1
         double total = 0;              //T(n) = 1
         for (int j=0; j < n; j++) {    //T(n) = n
-            total += x[j];              //T(n) = 1
-            a[j] = total / (j+1);       //T(n) = 1
+            total += x[j];             //T(n) = 1
+            a[j] = total / (j+1);      //T(n) = 1
         }
-        return a;                       //T(n) = 1
+        return a;                      //T(n) = 1
     }
 
     //T(n)=n + 6
     //O(n)     Linear-time algorithm
+
+
+    public  static  void main(String[] args){
+
+    }
 }

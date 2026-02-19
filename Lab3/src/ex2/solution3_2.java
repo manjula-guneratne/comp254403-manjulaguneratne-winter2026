26c9f5167185c39cabe49c0832748f45b331bbb7
Manjula Guneratne <mguneratne3@gmail.com>
2026-02-19 18:34:17 -0500

    Lab3 solution3_2 completed

---
+++ Lab3/src/ex2/solution3_2.java
@@ -0,0 +1,31 @@
+package ex2;
+import java.util.Scanner;
+
+public class solution3_2 {
+
+    public static boolean isPalindrome(String s) {
+
+        if (s.length() <= 1)
+            return true;
+
+        if (s.charAt(0) != s.charAt(s.length() - 1))
+            return false;
+
+        return isPalindrome(s.substring(1, s.length() - 1));
+    }
+
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter a string: ");
+        String word = input.nextLine();
+
+        if (isPalindrome(word))
+            System.out.println("It is a palindrome.");
+        else
+            System.out.println("It is NOT a palindrome.");
+
+        input.close();
+    }
+}
